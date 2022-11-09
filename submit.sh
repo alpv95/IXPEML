b7f841a64d0ed0b6a9ba08c3bc3a5df20ae6838c
alpv95 <59vpla@gmail.com>
2022-11-09 12:04:11 -0800

    NN IXPE l1->l2 pipeline

--- submit.sh
+++ submit.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 #
 #SBATCH --job-name=GPUtest
-#SBATCH --time=120:00
+#SBATCH --time=80:00
 #SBATCH --ntasks=1
 ##SBATCH --cpus-per-task=2
-#SBATCH --mem=50G
+#SBATCH --mem=16G
 #SBATCH --partition=owners
-#SBATCH --gres gpu:1
+#SBATCH --gres gpu:2
 #SBATCH -C GPU_MEM:16GB
-#SBATCH -C GPU_BRD:GEFORCE
-#SBATCH -C GPU_SKU:RTX_2080Ti
-#SBATCH -C GPU_CC:7.5
-
-ml gsl
-ml python/3.9
-ml py-scipy/1.6.3_py39
-ml viz
-ml py-matplotlib/3.4.2_py39
-ml cmake/3.13.1
-ml py-numpy/1.20.3_py39
-ml cudnn/8.1.1.33
-ml py-pytorch/1.8.1_py39
-ml cuda/11.1.1
+##SBATCH -C GPU_BRD:GEFORCE
+##SBATCH -C GPU_SKU:RTX_2080Ti
+##SBATCH -C GPU_CC:7.5
 
 N="9"
+DATA="09_det1"
 
 nvidia-smi
-srun python3 run_ensemble_eval.py heads_only_choice --data_list spectra_68720/test_aug6/train/ --ensemble heads_only_new --datatype sim --batch_size 512
+python3 run_ensemble_eval.py $DATA --data_list data/mrk421/"$DATA"/ --batch_size 512
